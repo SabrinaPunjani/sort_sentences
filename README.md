d448103016722e71c97fd9065386964b94b92c13
Sabrina <43822163+SabrinaPunjani@users.noreply.github.com>
2023-03-06 19:38:55 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # sort_sentences
 
-given a story in short_story.txt file sort them in alphabetical order and create a new text file with sorted story 
+This program takes a given story in the same folder named ShortStory.txt and sorts the sentences into alphabetical order to create a new text file SortedShortStory.txt containing the result.
+
+--------------------------------Program Requirements--------------------------------
+
+This program requires Python3 to run avaliable at https://www.python.org/downloads/ 
+
+This program also requires the Natrual Language Toolkit library which can be downloaded using 'pip3 install nltk' on Windows or 'pip install --user -U nltk' on Linux/Unix
+
+
+--------------------------------Running the Program--------------------------------
+
+Ensure that ShortStory.txt exists in the same root file directory ~\sort_sentences
+
+Run 'run.bat' file 
+
+If the script does not work, the file story_sorter.py can simply be run with Python
+
+
+
+
+
--- story_sorter.py
+++ story_sorter.py
@@ -1,21 +1,49 @@
 
+#Sabrina Punjani Story Sorter 2023
 
-filename = "ShortStory.txt"
+
+
+import nltk #natural language toolkit 
+
+# download the punkt tokenizer if necessary
+nltk.download('punkt')
+
+filename = 'ShortStory.txt'
 
 # open the input file
 with open(filename, 'r') as file:
     # read the contents of the file
     contents = file.read()
+    contents= contents.replace('------------------------------------------------','')
 
 # split the contents into a list of sentences
-sentences = contents.split('.')
+sentences = nltk.sent_tokenize(contents)
 
-# sort the sentences in alphabetical order
-sentences.sort()
+# sort the sentences in alphabetical order, we will ignore the first character of the string if it starts with a quote or bracket when sorting
+#lambda acts as a function passing through the string with the first index sliced off if the first index is " or ( or --
+sentences = sorted(sentences, key=lambda s: s[1:] if s.startswith('"') or s.startswith('(') else s)
+sentences = sorted(sentences, key=lambda s: s[2:] if s.startswith('--') else s)
+#ignore case when sorting
+sentences = sorted(sentences, key=lambda s: s.lower())
 
-# join the sorted sentences back into a string
-sorted_contents = '.'.join(sentences)
 
-# write the sorted contents to the output file
+
+# join the sorted sentences back into a nicely formatted string
+
+paragraph = 6 #new paragraph after this many sentences
+newsection = 20 #new section after this many sentences
+sorted_contents = ''
+
+for i in range(len(sentences)):
+    sorted_contents+=sentences[i]+' '
+    if i%paragraph==0:
+        sorted_contents+='\n'
+    if i%newsection ==0:
+        sorted_contents+='\n ------------------------------------------------ \n \n'
+     
+        
+
+# write the sorted contents into the output file
 with open('SortedShortStory.txt', 'w') as file:
     file.write(sorted_contents)
+
